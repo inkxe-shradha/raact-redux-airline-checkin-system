8a1dd5a38801e09ddfa52c800c4a0cf8a5c6b0ea
Shradha suman Praharaj <42928943+inkxe-shradha@users.noreply.github.com>
2022-10-04 10:14:36 +0530

    Update api.config.js
    Update the API config changes

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/";
+const BASE_URL = "http://localhost:3002/";
 const Axios = axios.create({
   baseURL: BASE_URL,
   timeout: 5000,
